8a75376d2ff4fe0f4c81e46d3ae3df17040bcf9c
Talon <2962803755@qq.com>
2017-03-25 08:19:54 +0800

    调整

--- AutolayoutCell/Class/JLStasus.h
+++ AutolayoutCell/Class/JLStasus.h
@@ -9,10 +9,25 @@
 #import <UIKit/UIKit.h>
 
 @interface JLStasus : NSObject
+/**
+名称
+ */
 @property (strong, nonatomic) NSString *name;
+/**
+ 内容
+ */
 @property (strong, nonatomic) NSString *text;
+/**
+ 头像
+ */
 @property (strong, nonatomic) NSString *icon;
+/**
+ 图片
+ */
 @property (strong, nonatomic) NSString *picture;
+/**
+ 是否是VIP
+ */
 @property (assign, nonatomic, getter=isVip) BOOL vip;
 
 /** cell的高度 */
